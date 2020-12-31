7c2d9b765c4cdc4727ff7c11aa366d1f1252cd5f
gohell990 <gohell69@hotmail.com>
2020-12-31 14:48:51 +0800

    Webapp Original

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-avatar
+        color="#FECC00"
+        size="56"
+        
+      >
+        XMIY
+      </v-avatar>
+    </v-app-bar>
+    <v-main>
+      <v-breadcrumbs
+        divider="/"
+        link
+      >
+        <v-breadcrumbs-item to="/">Home</v-breadcrumbs-item>
+        <v-breadcrumbs-item v-for="link in links" :key="link.path" :to="link.path" link>H{{link.path}}</v-breadcrumbs-item>
+      
+      </v-breadcrumbs>
+      
+    
+      <router-view></router-view>
+    </v-main>
+
+    
+    <v-bottom-navigation
+      
+      color="primary"
+    >
+      
+      
+      <v-btn icon to="/about">
+        <span>Recents</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+      <v-btn icon to="/">
+        <span>Favourites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon to="/home">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+      <v-btn icon to="/home">
+        <span>Account</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+     
+    </v-bottom-navigation>
+
+    
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default{
+    data (){
+      return {
+        navigations: [
+          { title:"Recents", icon:"mdi-history", to:'/about'},
+          { title:"Favourites", icon:"mdi-heart", to:'/'},
+          { title:"Nearby", icon:"mdi-map-marker", to:"/about"}
+        ],
+        links: [],
 
-#nav {
-  padding: 30px;
+      }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    },
+    methods:{
+      getRoute(){
+        this.links = this.$route.matched;
+        console.log(this.$route)
+      }
+    },
+    created(){
+      this.getRoute();
     }
   }
-}
-</style>
+
+</script>
